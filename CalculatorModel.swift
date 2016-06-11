7412f03034f9c21950b645eeb8a601fc264ee8ec
shiuaner <shiuaner@gmail.com>
2016-06-11 21:07:54 +0800

    L2 finished

--- CalculatorModel.swift
+++ CalculatorModel.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-func multiply(op1: Double, op2: Double) -> Double {
-	return op1 * op2
-}
-
 class CalculatorModel {
 	
 	private var accumulator = 0.0
@@ -23,9 +19,13 @@ class CalculatorModel {
 	private var operations: Dictionary<String, Operation> = [
 		"π": Operation.Constant(M_PI),
 		"e": Operation.Constant(M_E),
+		"±": Operation.UnaryOperation({-$0}),
 		"√": Operation.UnaryOperation(sqrt),
 		"cos": Operation.UnaryOperation(cos),
-		"×": Operation.BinaryOperation(multiply),
+		"+": Operation.BinaryOperation({$0 + $1}),
+		"-": Operation.BinaryOperation({$0 - $1}),
+		"×": Operation.BinaryOperation({$0 * $1}),
+		"÷": Operation.BinaryOperation({$0 / $1}),
 		"=": Operation.Equals
 	]
 	
@@ -39,14 +39,23 @@ class CalculatorModel {
 	func performOperation(symbol: String) {
 		if let operation = operations[symbol] {
 			switch operation {
-			case .Constant(let value): accumulator = value
-			case .UnaryOperation(let function): accumulator = function(accumulator)
-			case .BinaryOperation(let function):pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+			case .Constant(let value):
+				accumulator = value
+			case .UnaryOperation(let function):
+				accumulator = function(accumulator)
+			case .BinaryOperation(let function):
+				executePendingBinaryOperation()
+				pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
 			case .Equals:
-				if pending != nil {
-					accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
-				}
+				executePendingBinaryOperation()
 			}
+		}
+	}
+	
+	private func executePendingBinaryOperation() {
+		if pending != nil {
+			accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+			pending = nil
 		}
 	}
 	
